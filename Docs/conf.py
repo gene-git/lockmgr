1c21848e88bc0591e6639ee67d60b71084716e2f
Gene C <arch@sapience.com>
2024-10-19 18:03:49 -0400

    remove unused requirements file

--- Docs/conf.py
+++ Docs/conf.py
@@ -9,7 +9,7 @@
 project = "lockmgr"
 copyright = '2023, Gene C'
 author = 'Gene C'
-release = '1.4.0'
+release = '1.5.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
